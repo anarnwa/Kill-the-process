c76b4b3bf014c18f17fd02d068efda0cc33d666e
浩 杨 <597784375@163.com>
2018-07-18 18:15:42 +0800

    添加项目文件。

---
+++ 杀进程/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 杀进程
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process[] process = Process.GetProcessesByName(textBox1.Text);
+            foreach (Process p in process)
+            {
+                if (!p.HasExited)  // 如果程序没有关闭，结束程序
+                {
+                    p.Kill();
+                    p.WaitForExit();
+                }
+            }
+        }
+    }
+}
